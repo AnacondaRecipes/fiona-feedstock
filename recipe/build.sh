899952cb770f8d63c0ef61123604710e7cf89b90
Michael Sarahan <msarahan@gmail.com>
2017-11-07 12:35:02 -0600

    fix null value issue with gdal 2.2 (backport from 1.8)

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if [[ $(uname) == Darwin ]]; then
-  export LDFLAGS="-headerpad_max_install_names $LDFLAGS"
-fi
-
 $PYTHON setup.py build_ext -I$PREFIX/include -L$PREFIX/lib -lgdal \
                  install --single-version-externally-managed --record record.txt
