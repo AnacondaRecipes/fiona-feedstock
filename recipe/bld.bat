8bbd5c2eedbe0377920aa5061b1b178711719a46
ViridianMelody <mlove@anaconda.com>
2023-01-19 10:24:13 -0700

    Pin libgdal to 3.0.2, adjust bld.bat accordingly

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,6 +5,6 @@ set GDAL_VERSION=%%F
 if errorlevel 1 exit 1
 echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal_i -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
-if errorlevel 1 %PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
+:: Replace "-lgdal" with "-lgdal_i" if building with libgdal version greater than 3.0.2
+%PYTHON% setup.py build_ext -I"%LIBRARY_INC%" -lgdal -L"%LIBRARY_LIB%" install --single-version-externally-managed --record record.txt
 if errorlevel 1 exit 1
