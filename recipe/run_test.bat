cdc9d912991b90dbd349c948a243f11818cace1f
ViridianMelody <mlove@anaconda.com>
2023-01-20 09:47:33 -0700

    Move test commands to run_test files

--- recipe/run_test.bat
+++ recipe/run_test.bat
@@ -5,3 +5,6 @@ xcopy test_data\coutwildrnp.json tests\data /s /e /y || exit 1
 
 %PYTHON% -m pytest -s -rxs -v -m "not wheel" -k "not (test_fio_ls_single_layer or test_directory or test_directory_trailing_slash or test_options or test_transaction or test_no_append_driver_cannot_append[PCIDSK])" tests
 %PYTHON% -m pip check
+fio --help
+fio ls test_data/test.shp
+fio info test_data/test.shp
