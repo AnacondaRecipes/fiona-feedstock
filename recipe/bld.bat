e5aa6f40eb6dc1e2c8775dd933ecb977559bf354
Charles Bousseau <cbousseau@anaconda.com>
2023-03-02 17:46:06 -0500

    add --no-use-pep517

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -5,7 +5,7 @@ set GDAL_VERSION=%%F
 if errorlevel 1 exit 1
 echo "set GDAL_VERSION=%GDAL_VERSION%"
 
-%PYTHON% -m pip install --no-deps --ignore-installed . ^
+%PYTHON% -m pip install --no-deps --no-use-pep517 --ignore-installed . ^
                         --global-option=build_ext ^
                         --global-option="-I%LIBRARY_INC%" ^
                         --global-option="-L%LIBRARY_LIB%" ^
