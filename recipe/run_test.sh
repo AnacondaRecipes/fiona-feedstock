1a73adcf9912abd54c8119fcd9c3807794c8a444
TS-ANC <208963744+TS-ANC@users.noreply.github.com>
2026-06-17 15:55:48 +0200

    update test skips

--- recipe/run_test.sh
+++ recipe/run_test.sh
@@ -6,10 +6,22 @@ cp -r tests /tmp/
 
 pushd /tmp
 
-# Skip test_open_repr and test_open_s3 because of the following errors:
-# - test_open_repr: fiona.errors.DriverError: Failed to open dataset (flags=68): /vsizip//private/tmp/tests/data/coutwildrnp.zip/coutwildrnp.shp
-# - test_open_s3: fiona.errors.DriverError: Failed to open dataset (flags=68): /vsizip/vsis3/fiona-testing/coutwildrnp.zip
-$PYTHON -m pytest -s -rxs -v -m "not network"  -m "not wheel" -k "not (test_listdir_zipmemoryfile or test_open_repr or test_open_s3)" tests
+skip_tests=()
+
+# returned json with GDAL 3.13 has different whitespace formatting than previous version which fiona's tests
+# assert exactly against. tests can be restored once resolved: https://github.com/Toblerity/Fiona/issues/1512
+skip_tests+=(--deselect=tests/test_collection.py::test_open_kwargs)
+skip_tests+=(--deselect=tests/test_subtypes.py::test_write_bool_subtype)
+
+# known GeoJSON driver limitation, which since v3.13 also manifests itself as test failure. restore once fixed upstream.
+# E Failed to remove datasource. fiona.errors.DatasetDeleteError: Driver does not support dataset removal operation
+skip_tests+=(--deselect=tests/test_fio_rm.py::test_remove[GeoJSON])
+skip_tests+=(--deselect=tests/test_remove.py::test_remove[GeoJSON-path-True])
+skip_tests+=(--deselect=tests/test_remove.py::test_remove[GeoJSON-path-False])
+skip_tests+=(--deselect=tests/test_remove.py::test_remove[GeoJSON-collection-True])
+skip_tests+=(--deselect=tests/test_remove.py::test_remove[GeoJSON-collection-False])
+
+$PYTHON -m pytest -s -rxs -v -m "not network"  -m "not wheel" "${skip_tests[@]}" tests
 popd
 $PYTHON -m pip check
 fio --help
